81805b83aba975d9b741c4865e02dfd3e8584002
SivaKumar Pachuri <SivaKumar Pachuri@LAPTOP-HNM6OKVU>
2023-09-04 18:34:41 +0530

    second commit here

--- cmdproject/src/main/java/com/App.java
+++ cmdproject/src/main/java/com/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+        //so here let me think here
         //this is the one of the sample project here .`
     }
 }
